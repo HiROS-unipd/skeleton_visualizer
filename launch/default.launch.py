c561747b613101aadd21bc1613ed6bde1a702a11
Mattia Guidolin <mattia.guidolin@gmail.com>
2022-11-04 15:39:35 +0100

    Use single quotes in launch file

--- launch/default.launch.py
+++ launch/default.launch.py
@@ -4,19 +4,19 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package="hiros_skeleton_visualizer",
-            executable="hiros_skeleton_visualizer",
-            name="skeleton_visualizer",
-            namespace="hiros",
-            output="screen",
+            package='hiros_skeleton_visualizer',
+            executable='hiros_skeleton_visualizer',
+            name='skeleton_visualizer',
+            namespace='hiros',
+            output='screen',
             emulate_tty=True,
             parameters=[
-                {"input_topic": "/input/topic"},
-                {"output_topic": "/output/topic"},
-                {"publish_tfs": False},
-                {"seed": 0},
-                {"lifetime": 0.1},
-                {"alpha": 1.},
+                {'input_topic': '/input/topic'},
+                {'output_topic': '/output/topic'},
+                {'publish_tfs': False},
+                {'seed': 0},
+                {'lifetime': 0.1},
+                {'alpha': 1.},
             ]
         )
     ])
